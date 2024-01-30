3b6f1b847b85197f9c49204953c850f43656a7cb
Grym1314 <chucho_031199@hotmail.com>
2024-01-29 19:15:36 -0600

    Changing favicon icon

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Grym1314"
           target="_blank"
           rel="noopener noreferrer"
         >
-          New thing
+          My Github Profile
         </a>
       </header>
     </div>
