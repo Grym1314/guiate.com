b143a2811c2734d5c2ee451ba254264d4d9a60a6
Grym1314 <chucho_031199@hotmail.com>
2024-02-29 20:32:47 -0600

    GUIATE banner section 02 29 24

--- src/views/LandingPage/index.js
+++ src/views/LandingPage/index.js
@@ -1,19 +1,21 @@
 import './LandingPage.css';
+import Header from './sections/Header';
+
 
 const LandingPage = () => {
   return (
-				<section id="banner">
-					<div class="content">
-						<header>
-							<h2>The future has landed</h2>
-							<p>And there are no hoverboards or flying cars.<br />
-							Just apps. Lots of mother flipping apps.</p>
-						</header>
-						<span class="image"><img src="images/pic01.jpg" alt="" /></span>
-					</div>
-					<a href="#one" class="goto-next scrolly">Next</a>
-				</section>
-    </div>
+    <><Header />
+    <section id="banner">
+      <div class="content">
+        <header>
+          <h2>The future has landed</h2>
+          <p>And there are no hoverboards or flying cars.<br />
+            Just apps. Lots of mother flipping apps.</p>
+        </header>
+        <span class="image"><img src="images/pic01.jpg" alt="" /></span>
+      </div>
+      <a href="#one" class="goto-next scrolly">Next</a>
+    </section></>
   );
 };
 
--- src/views/LandingPage/sections/Banner/index.js
+++ src/views/LandingPage/sections/Banner/index.js
@@ -1,11 +1,17 @@
-<section id="banner">
-<div class="content">
-    <header>
-        <h2>The future has landed</h2>
-        <p>And there are no hoverboards or flying cars.<br />
-        Just apps. Lots of mother flipping apps.</p>
-    </header>
-    <span class="image"><img src="images/pic01.jpg" alt="" /></span>
-</div>
-<a href="#one" class="goto-next scrolly">Next</a>
-</section>
+import './banner.css';
+
+const Banner = () =>{
+    <div className="banner">
+        <div className="content">
+            <header>
+                <h2>The future has landed</h2>
+                <p>And there are no hoverboards or flying cars.<br />
+                Just apps. Lots of mother flipping apps.</p>
+            </header>
+            <span className="image"><img src="images/pic01.jpg" alt="" /></span>
+        </div>
+        <a href="#one" className="goto-next scrolly">Next</a>
+    </div>
+}
+
+export default Banner;
