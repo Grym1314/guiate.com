4451aa851a6b98da1d82b4a8685f9b329e6b28ee
Grym1314 <chucho_031199@hotmail.com>
2024-03-03 20:16:33 -0600

    Adding new way to render my LandingPage

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import LandingPage from './views/LandingPage';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <Route path="/" element={<LandingPage />} />
-    </Router>
-  </React.StrictMode>
-);
+const el = document.getElementById('id');
+const root = ReactDOM.createRoot(el);
+const App = () =>{
+  return(
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
+
+root.render(<App/>);
